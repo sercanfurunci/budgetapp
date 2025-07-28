7937d9f4cf2c235a55277c6df69f290d8abbc4a7
Sercan Furunci <sercanfurunci41@gmail.com>
2025-07-28 18:20:07 +0300

    fix back

--- server/index.js
+++ server/index.js
@@ -2,39 +2,40 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
+
 const path = require("path");
+
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Tüm gelen istekleri logla (API request’leri için)
+// Tüm gelen istekleri logla
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.originalUrl}`);
   next();
 });
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
 // Route dosyalarını import et
 const authRoutes = require("./routes/authRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
+
+
 
 // DB bağlantısı yap ve sonra serverı aç
 connectDB()
   .then(() => {
     console.log("MongoDB connected ✅");
 
-    // Routes (API’ler önce tanımlanmalı)
+    // Routes
     app.use("/api/auth", authRoutes);
     app.use("/api/transactions", transactionRoutes);
 
-    // React build klasörünü statik olarak servis et
-    app.use(express.static(path.join(__dirname, "../client/build")));
-
-    // React SPA için tüm diğer GET isteklerini index.html’ye yönlendir
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    app.get("/", (req, res) => {
+      res.send("Budget API is running");
     });
 
     const PORT = process.env.PORT || 5000;
